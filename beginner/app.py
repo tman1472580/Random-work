c174b1fcca54574e2953f5e55f26ff167d8b1d8a
tman1472580 <67373632+tman1472580@users.noreply.github.com>
2023-11-29 11:43:39 +0000

    commits

--- beginner/app.py
+++ beginner/app.py
@@ -14,9 +14,24 @@ print(max_num(300,40,5))
 import useful_tools
 
 print(useful_tools.roll_dice(10))
-'''
+
 from Student import Student
 
 student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Bob", "Art", 3.8, False)
 
 print(student1.name)
+
+print(student2.on_honor_roll())
+'''
+
+from chef import Chef
+from chineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+
+
